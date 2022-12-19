e0b6560fbdb7c04c7fae526223f4a1dcd7250b96
DainiusMI <mental.def@gmail.com>
2022-12-19 18:36:29 +0200

    data file initial stucture

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 
-
+import buttonData from "./data"
 
 export default function App() {
 
-
+  const [displayScreen, setDisplayScreen] = React.useState({
+    fullLine: "",
+    currentValue: ""
+  })
+  console.log(buttonData.numeric)
   return (
     <div className="App">
     
+      <section id="display" className="screen-container"> 
+        <p className="fullLine">{displayScreen.fullLine}</p>
+        <p className="currentValue">{displayScreen.currentValue}</p>
+      </section>
+
+      <section className="button-container">
+
+      </section>
     </div>
   )
 }
