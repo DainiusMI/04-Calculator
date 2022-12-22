3cd2b1e400203f8cff5d09c01872ad64f7250979
DainiusMI <mental.def@gmail.com>
2022-12-22 13:46:13 +0200

    big commit

--- src/assets/components/Buttons.jsx
+++ src/assets/components/Buttons.jsx
@@ -9,6 +9,9 @@ export default function Buttons(props) {
     const lastSymbol = displayScreen.displayLine[displayScreen.displayLine.length-1];
     const secondToLastSymbol = displayScreen.displayLine[displayScreen.displayLine.length-2];
 
+    const lastOfDisplayLine = displayScreen.displayLine[displayScreen.displayLine.length -1]
+    const secondToLastOfDisplayLine = displayScreen.displayLine[displayScreen.displayLine.length -2]
+    
     const [displaySymbols, setDisplaySymbols] = React.useState()
     React.useEffect(() => {
         setDisplaySymbols({
@@ -24,35 +27,44 @@ export default function Buttons(props) {
     function handleNumbers(event) {
         const value = event.target.value
 
-        function besicSetDisplay() {
+        function setTemplate() {
             setDisplayScreen(prevData => ({
                 displayLine: prevData.displayLine + value,
                 displayValue: prevData.displayValue + value
             }))
         }
 
+        if (/\+|\-|\*|\//.test(lastOfDisplayLine)) {
+                setDisplayScreen(prevData => ({
+                    displayLine: prevData.displayLine + value,
+                    displayValue: value
+                }))
+        }
+        /*
         if ( /\+|\-|\*|\//.test(displayScreen.displayValue)) {
+            console.log("tic")
             setDisplayScreen(prevData => ({
-                displayLine: prevData.displayLine + value,
+                displayLine: displayScreen.displayLine === "0" ? value : prevData.displayLine + value,
                 displayValue: value
             }))
         }
+        */
         // not decimal point was pressed
-        else if (value !== ".") { 
-            if (displayScreen.displayValue == "0") {
-                setDisplayScreen(prevData => ({
+        else  if (value !== ".") { 
+            if (displayScreen.displayValue === "0") {
+                setDisplayScreen({
                     displayLine: value,
-                    displayValue: prevData.displayValue.slice(1) + value
-                }))
+                    displayValue: value
+                })
             }
             else {
-                besicSetDisplay()
+                setTemplate()
             }
         }
         // decimal point was pressed
         else {
             if (displayScreen.displayValue.includes(".") === false) {
-                besicSetDisplay()
+                setTemplate()
             }
         }
 
@@ -63,22 +75,73 @@ export default function Buttons(props) {
     function handleFunctions(event) {
         const {id, value} = event.target
 
-        
         function setTemplate() {
-            if (resultData.result === "") {
-                setDisplayScreen(prevData => ({
-                    displayLine: prevData.displayLine == "0" ? 
-                        prevData.displayValue + value : 
-                        prevData.displayLine + prevData.displayValue + value,
-                    displayValue: "0"
-                }))
+            setDisplayScreen(prevData => ({
+                displayLine: prevData.displayLine + value,
+                displayValue: prevData.displayValue + value
+            }))
+        }
+        
+        function functionsTempalte() {
+
+            // handle minus 
+            if (value === "-") {
+                if (displayScreen.displayLine === "0") {
+                    setDisplayScreen({
+                        displayLine: value,
+                        displayValue: value
+                    })
+                }
+                // if last symbol is a function and second to last is not allow to put negative sign next
+                else if (/\+|\-|\*|\//.test(lastOfDisplayLine) && !/\+|\-|\*|\//.test(secondToLastOfDisplayLine)) {
+                    setDisplayScreen(prevData => ({
+                        displayLine: prevData.displayLine + value,
+                        displayValue: prevData.displayValue + value
+                    }))
+                }
+                else if (!/\+|\-|\*|\//.test(lastOfDisplayLine)) {
+                    setDisplayScreen(prevData => ({
+                        displayLine: prevData.displayLine === "0" ? 
+                           // prevData.displayValue + value : 
+                            value : 
+                            prevData.displayLine + value,
+                        displayValue: value
+                    }))
+                }
             }
+            // handle other function signs
             else {
-                setDisplayScreen({
-                    displayLine: resultData.result + value,
-                    displayValue: "0"
-                })
+                // change the fucntion sign if last symbol is a function sign and secont to last is not
+                // do not allowing to change negative signs in front of displayLine
+                if (displayScreen.displayLine !== "-" && /\+|\-|\*|\//.test(lastOfDisplayLine) && !/\+|\-|\*|\//.test(secondToLastOfDisplayLine)) {
+                    setDisplayScreen(prevData => ({
+                        displayLine: prevData.displayLine.slice(0, -1) + value,
+                        displayValue: prevData.displayValue.slice(0, -1) + value
+                    }))
+                }
+
+                else if (!/\+|\-|\*|\//.test(lastOfDisplayLine)) {
+                    if (resultData.result === "") {
+                        setDisplayScreen(prevData => ({
+                            displayLine: prevData.displayLine === "0" ? 
+                               // prevData.displayValue + value : 
+                                value : 
+                                prevData.displayLine + value,
+                            displayValue: value
+                        }))
+
+                    }
+                    else {
+                        setDisplayScreen({
+                            displayLine: resultData.result + value,
+                            displayValue: value
+                        })
+                    }
+
+                }
             }
+
+
         }
 
         
@@ -89,19 +152,25 @@ export default function Buttons(props) {
                 setDisplayScreen(initialScreenState)
                 setResultData(initialResultData)
             break;
+
             case "add":
-                setDisplayScreen(prevData => ({
-                    displayLine: prevData.displayLine + value,
-                    displayValue: value
-                }))
+                functionsTempalte()
             break;
-            
-            case "equals":
 
-               
+            case "subtract":
+                functionsTempalte()
+            break;
 
+            case "multiply":
+                functionsTempalte()
+            break;
+
+            case "divide":
+                functionsTempalte()
+            break;
+
+            case "equals":            
                 //const url = "http://api.mathjs.org/v4/?expr=" + displayScreen.displayLine.replaceAll("+", "%2B").replaceAll("/", "%2F")
-
                 fetch(resultData.url).then(resp => resp.json()).then(data => {
                     console.log("data: "+ data)
                     setResultData(prevData => ({
@@ -114,7 +183,6 @@ export default function Buttons(props) {
                         displayValue: data
                     }))
                 })
-                
             break;
 
         }
